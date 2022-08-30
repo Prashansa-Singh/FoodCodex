1ff4c307e6b513cf43e40c5686835f93b5e96167
yipp-217 <yipp.217@gmail.com>
2022-08-30 19:58:32 +1000

    Setup basic Express app

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-// Import Express
-const express = require('express');
+//
+//
+//
 
-// Set up app as express app
+const express = require('express');
+const flash = require('express-flash')
+
 const app = express();
 
-// Link router
-const homeRouter = require('./routes/homeRouter');
-app.use('/', homeRouter);
+app.use(flash())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
+/* ------------------------------------------------------------------------- */
+/* debugger, log message */
+app.use((req, res, next) => {
+    console.log('msg: ' + req.method + req.path)
+    next()
+})
+
+/* ------------------------------------------------------------------------- */
 const PORT = process.env.PORT || 8080;
 
+/* Environment */
+// if ()...
+
+/* Routers */
+const homeRouter = require('./routes/homeRouter');
+
+
+
+/* ========================================================================= */
+// Link router
+app.use('/', homeRouter);
+
+
+
+/* ------------------------------------------------------------------------- */
 app.listen(PORT, () => {
-    console.log("App is listening on port 8080")
+    console.log("App is listening on port" + PORT);
 });
