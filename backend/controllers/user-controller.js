c8040f24a2b338a2a459c622f89fc1f5653d471c
yipp-217 <yipp.217@gmail.com>
2022-09-01 16:35:34 +1000

    Signup (create) user and save to db

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -2,18 +2,23 @@
 const path = require('path');
 
 const {User} = require('../models/user')
-const {Restaurant} = require('../models/restaurant')
+
 
 const getSignup = async (req, res) => {
+    // render it later, now just send html file
     res.sendFile(path.join(__dirname+'/../views/signup.html'));
 }
 
 const createUser = async (req, res, next) => {
     try {
-        // const user = new User(req.json.body)
-        // await user.save()
-        // res.json(req.body)
-        return res.redirect('/')
+        const user = new User(req.body)
+
+        // visualise
+        console.log(req.body)
+        console.log(user)
+
+        await user.save()
+        return res.redirect('/user/signup')
     }
     catch (err) {
         return next(err)
