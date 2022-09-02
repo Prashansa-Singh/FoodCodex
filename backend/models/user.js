947355d804ed768300ef32740e3c6c9d9750abfc
yipp-217 <yipp.217@gmail.com>
2022-09-02 23:10:02 +1000

    Remove darkMode from user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     userName: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-
     displayName: {type: String, required: true},
-    darkMode: {type: Boolean, default: false},
-
     restaurants: {type: [mongoose.Types.ObjectId], ref: 'Restaurant', default: []},
 
     // secret: {type: String, required: true}  // for cookie
