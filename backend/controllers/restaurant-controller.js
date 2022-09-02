f1872d4c64868ef8aaf53ad4e71ffa6ed7204aff
yipp-217 <yipp.217@gmail.com>
2022-09-02 17:29:32 +1000

    Create restaurant and add to user's restaurants

--- backend/controllers/restaurant-controller.js
+++ backend/controllers/restaurant-controller.js
@@ -1,17 +1,24 @@
+const {User} = require('../models/user')
 const {Restaurant} = require('../models/restaurant')
 
 const getRestaurants = async (req, res) => {
-    //
+    // not complete yet
+    const restaurants = await User.findOne(
+        {_id: req.params.userId}).populate("restaurants");
 }
 
 const createRestaurant = async(req, res, next) => {
     try {
         const restaurant = new Restaurant(req.body)
+        await restaurant.save()
+        await User.updateOne(
+            {_id: req.params.userId},
+            {$push: {restaurants: restaurant._id}}
+        )
 
         console.log(req.body)
         console.log(restaurant)
 
-        await restaurant.save()
         return res.redirect('/')
     }
     catch (err) {
