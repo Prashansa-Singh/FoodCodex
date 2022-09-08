d63fb47b91c1147b028ca103361ad9cce825b54e
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-09-08 21:18:34 +1000

    Add icons to navigation

--- frontend/components/navigation.js
+++ frontend/components/navigation.js
@@ -7,46 +7,59 @@ export default function Nav() {
 
     const navItems = [
         {
-            href: '/',
-            title: 'Home',
-        },
-        {
-            href: '/about',
-            title: 'About',
-        },
-        {
-            href: '/login',
-            title: 'Login',
-        },
-        {
-            href: '/signup',
-            title: 'Sign Up',
-        },
-        {
             href: '/restaurant-collection/view-restaurant-collection',
-            title: 'View Restaurant Collection',
-        },
-        {
-            href: '/restaurant-collection/view-restaurant-record',
-            title: 'View Restaurant Record',
+            title: 'Home',
+            icon: '/src/nav-icons/home-icon.svg',
         },
         {
             href: '/restaurant-collection/edit-restaurant-record',
-            title: 'Edit Restaurant Record',
+            title: 'Add Restaurant',
+            icon: '/src/nav-icons/add-edit-nav-icon.svg',
+        },
+        {
+            href: '/',
+            title: 'Customise Tags',
+            icon: '/src/nav-icons/tags-icon.svg',
+        },
+        {
+            href: '/',
+            title: 'Share My List',
+            icon: '/src/nav-icons/share-icon.svg',
+        },
+        {
+            href: '/',
+            title: 'Shared With Me',
+            icon: '/src/nav-icons/share-with-me-icon.svg',
         },
         {
             href: '/restaurant-collection/settings',
             title: 'Settings',
+            icon: '/src/nav-icons/settings-icon.svg',
+        },
+        {
+            href: '/about',
+            title: 'About Us',
+            icon: '/src/nav-icons/about-us-icon.svg',
+        },
+        {
+            href: '/',
+            title: 'Logout',
+            icon: '/src/nav-icons/logout-icon.svg',
         }
     ];
 
     return (
         <nav className={styles.nav}>
             <ul className={styles.navlist}>
-                {navItems.map(({href, title}) => (
+                {navItems.map(({href, title, icon}) => (
                     <li className={` ${styles.unselected} ${router.asPath === href && styles.selected}`}>
                         <Link href={href}>
-                            {title}
+                            <a>
+                                <div className={styles.icons}>
+                                    {title}
+                                    <img src={icon} />
+                                </div>
+                            </a>
                         </Link>
                     </li>
                 ))}
