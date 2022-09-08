1b3ade5e1e5437a9f27da17d9ef3383e2e57fce8
Prashansa-Singh <88768231+Prashansa-Singh@users.noreply.github.com>
2022-09-08 16:26:02 +1000

    Change user route to avoid Type error

--- backend/routes/restaurant-router.js
+++ backend/routes/restaurant-router.js
@@ -3,9 +3,9 @@ const express = require('express')
 const restaurantRouter = express.Router()
 const restaurantController = require('../controllers/restaurant-controller')
 
-restaurantRouter.get('/:userId/', restaurantController.getRestaurants)
+restaurantRouter.get('/retrieve/:userId/', restaurantController.getRestaurants)
 
-restaurantRouter.post('/:userId/restaurants/create-restaurant', restaurantController.createRestaurant)
+restaurantRouter.post('/retrieve/:userId/restaurants/create-restaurant', restaurantController.createRestaurant)
 
 
 module.exports = restaurantRouter;
