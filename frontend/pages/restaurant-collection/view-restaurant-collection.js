6a96eb84bcb3ecee410b17a3e64137f1ceb6615d
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-09-13 17:29:31 +1000

    Set up basic layout for View Restaurant Collection Page

--- frontend/pages/restaurant-collection/view-restaurant-collection.js
+++ frontend/pages/restaurant-collection/view-restaurant-collection.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../../components/layout';
 import utilStyles from '../../styles/utils.module.css';
+import styles from '../../styles/view-restaurant-collection.module.css'
 
 export default function ViewRestaurantCollection() {
 	return (
@@ -10,12 +11,47 @@ export default function ViewRestaurantCollection() {
 			</Head>
 			<section className={utilStyles.headingMd}>
 				<h1>
-					View Restaurant Collection
+					Restaurant Collection
 				</h1>
 
-				<p>
-					Display all the restaurants of the user
-				</p>
+				<div className={styles.collection_container}>
+					<input 
+						className={styles.searchbar}
+						id='searchbar'
+						type="search" 
+						name="search"
+						placeholder="Search..." 
+					/>
+					<div className={styles.filter}>
+						<div className={styles.filter_icon_heading}>
+							<img className={styles.icon} src='/src/nav-icons/filter-icon.svg' alt='Filter Icon' />
+							<h4>Filter</h4>
+						</div>
+						<div className={styles.filter_options}>
+							<p>Filter will go here</p>
+						</div>
+					</div>
+					<div className={styles.table_container}>
+						<table className={styles.table}>
+							<thead>
+								<tr>
+									<th className={styles.th}>Name</th>
+									<th className={styles.th}>Rating</th>
+									<th className={styles.th}>Label</th>
+									<th className={styles.th}>Price</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr className={styles.tr}>
+									<td className={styles.td}>Example</td>
+									<td className={styles.td}>Example</td>
+									<td className={styles.td}>Example</td>
+									<td className={styles.td}>Example</td>
+								</tr>
+							</tbody>	
+						</table>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	);
