3a8548054fe4c2ff2eb2dbd20294a529190edc42
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-09-15 11:52:05 +1000

    Set up NODE_ENV

--- frontend/pages/api/axiosConfig.js
+++ frontend/pages/api/axiosConfig.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-//(process.env.NODE_ENV == "production") ? 'https://comp30022-programming-quokkas.herokuapp.com/' : 'http://localhost:8000/' ,
-
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/' ,
+    baseURL: (process.env.NODE_ENV == "production") ? 'https://comp30022-programming-quokkas.herokuapp.com/' : 'http://localhost:8000/' ,
 });
 
 export {axiosInstance};
