bf5b3287b37a94e1007c82eed83132bb3e739b0c
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-09-17 23:22:52 +1000

    Do basic layout for view record page

--- frontend/pages/restaurant-collection/view-restaurant-record.js
+++ frontend/pages/restaurant-collection/view-restaurant-record.js
@@ -24,47 +24,54 @@ export default function ViewRestaurantRecord({restaurant_data}) {
 				<title>{`${siteTitle} - ${restaurant_data.name}`}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<div className={styles.icon_group}>
-					<div className={styles.icons}>
-						<img src='/src/nav-icons/share-icon.svg' width='40vw' />
-						<p>Share</p>
+				<div className={styles.top}>	
+					<h1>
+						{restaurant_data.name}
+					</h1>
+					<div className={styles.icon_group}>
+						<div className={styles.icons}>
+							<img src='/src/nav-icons/share-icon.svg' width='40vw' />
+							<p>Share</p>
+						</div>
+						<div className={styles.icons}>
+							<img src='/src/nav-icons/add-edit-nav-icon.svg' width='40vw' />
+							<p>Edit</p>
+						</div>
 					</div>
-					<div className={styles.icons}>
-						<img src='/src/nav-icons/add-edit-nav-icon.svg' width='40vw' />
-						<p>Edit</p>
+				</div>
+
+				<div className={styles.restaurant_details}>
+					<p>
+						<i>{restaurant_data.cuisine}</i>
+					</p>
+
+					<p>
+						<i>{restaurant_data.address}</i>
+					</p>
+				</div>
+
+				<div className={styles.view_record_container}>
+					<div className={`${styles.data_container} ${styles.data1}`}>
+						<h5>Rating (out of 5 stars)</h5>
+						<p>{restaurant_data.rating}</p>
 					</div>
+
+					<div className={`${styles.data_container} ${styles.data2}`}>
+						<h5>Price Category</h5>
+						<p>{restaurant_data.priceRating}</p>
+					</div>
+
+					<div className={`${styles.data_container} ${styles.data3}`}>
+						<h5>Experiences</h5>
+					</div>
+
+					<div className={`${styles.data_container} ${styles.data4}`}>
+						<h5>Tags</h5>
+					</div>
+					<br/>
+					<br/>
+					<br/>
 				</div>
-
-				<h1>
-					{restaurant_data.name}
-				</h1>
-
-				<p>
-					<i>{restaurant_data.cuisine}</i>
-				</p>
-
-				<p>
-					<i>{restaurant_data.address}</i>
-				</p>
-
-				<div>
-					<h5>Rating (out of 5 stars)</h5>
-					<p>{restaurant_data.rating}</p>
-				</div>
-
-				<div>
-					<h5>Price Category</h5>
-					<p>{restaurant_data.priceRating}</p>
-				</div>
-
-				<div>
-					<h5>Tags</h5>
-				</div>
-
-				<div>
-					<h5>Experiences</h5>
-				</div>
-
 			</section>
 		</Layout>
 	);
