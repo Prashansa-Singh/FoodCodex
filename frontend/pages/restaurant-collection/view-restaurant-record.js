af14c4b51dd95142eaaf0ebc1819e0613bbcf3db
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-09-17 18:32:00 +1000

    Add basic layout to View Record page

--- frontend/pages/restaurant-collection/view-restaurant-record.js
+++ frontend/pages/restaurant-collection/view-restaurant-record.js
@@ -1,9 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../../components/layout';
 import utilStyles from '../../styles/utils.module.css';
-
+import styles from '../../styles/view-restaurant-record.module.css';
 import {axiosInstance} from '../api/axiosConfig';
-import axios from 'axios';
 
 export async function getServerSideProps({query}) {
 
@@ -22,16 +21,50 @@ export default function ViewRestaurantRecord({restaurant_data}) {
 	return (
 		<Layout>
 			<Head>
-				<title>{siteTitle}</title>
+				<title>{siteTitle} - {restaurant_data.name}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
+				<div>
+					<div>
+						<img src='/src/nav-icons/share-icon.svg' />
+						<p>Share</p>
+					</div>
+					<div>
+						<img src='/src/nav-icons/add-edit-nav-icon.svg' />
+						<p>Edit</p>
+					</div>
+				</div>
+
 				<h1>
 					{restaurant_data.name}
 				</h1>
 
 				<p>
-					Page displaying single restaurant record
+					<i>{restaurant_data.cuisine}</i>
 				</p>
+
+				<p>
+					<i>{restaurant_data.address}</i>
+				</p>
+
+				<div>
+					<h5>Rating (out of 5 stars)</h5>
+					<p>{restaurant_data.rating}</p>
+				</div>
+
+				<div>
+					<h5>Price Category</h5>
+					<p>{restaurant_data.priceRating}</p>
+				</div>
+
+				<div>
+					<h5>Tags</h5>
+				</div>
+
+				<div>
+					<h5>Experiences</h5>
+				</div>
+
 			</section>
 		</Layout>
 	);
