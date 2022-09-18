235445609003f9e26732f21af85e0171ff28feb6
callmekelvin <calvin.ho1@outlook.com>
2022-09-18 13:52:16 +1000

    Fixing Backend Production Routes

--- frontend/pages/api/axiosConfig.js
+++ frontend/pages/api/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: (process.env.NODE_ENV == "production") ? 'https://comp30022-programming-quokkas.herokuapp.com/' : 'http://localhost:8000/' ,
+	baseURL:
+		process.env.NODE_ENV == 'production'
+			? 'https://programming-quokkas-backend.herokuapp.com/'
+			: 'http://localhost:8000/',
 });
 
-export {axiosInstance};
+export { axiosInstance };
