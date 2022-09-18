00a1740ddaddae67e470a033b6c4037ee2ede5b2
yipp-217 <yipp.217@gmail.com>
2022-09-18 19:37:19 +1000

    Add home
    aboutUs and contactUs

--- backend/routes/home-router.js
+++ backend/routes/home-router.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const homeRouter = express.Router();
-// controller here
+const homeController = require('../controllers/home-controller')
 
 
-homeRouter.get('/', (req, res) => {
-    res.send("App is working")
-});
+homeRouter.get('/', homeController.status200);
 
+homeRouter.get('/about', homeController.aboutUs);
+
+homeRouter.get('/contact', homeController.contactUs);
 
 
 module.exports = homeRouter;
