9f9ab44f864cd6f6cf7f22ebedf2f70d5ca215a9
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-09-20 01:07:01 +1000

    made first draft of log in page

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -3,8 +3,43 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import LoginButton from '../components/loginbutton';
 
+import {axiosInstance} from './api/axiosConfig';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
 export default function Login() {
+	
+	const router = useRouter();
+
+	const submitUser = async (event) => {
+		event.preventDefault();
+		const userName = event.target.userName.value;
+		const password = event.target.password.value;
+		const displayName = event.target.displayName.value;
+
+		const body = {
+			userName: userName,
+			password: password,
+			displayName: displayName,
+		};
+
+		const url = '/account/signup';
+
+		axiosInstance.post(url, body)
+		.then(function (response) {
+			console.log(response.data);
+			router.push('/login');
+		})
+		.catch(function (error) {
+			console.log(error);
+		});	
+	
+	}
+
 	const title = `${siteTitle} - Login`;
+
+
+
 	return (
 		<Layout home>
 			<Head>
@@ -13,7 +48,19 @@ export default function Login() {
 			<section className={utilStyles.headingMd}>
 				<h1>Login</h1>
 
-				<p>Login page for the app</p>
+				<form onSubmit={submitUser}>
+					<label> Enter a user name </label>
+					<input type="text" placeholder="Username" name="userName" required/>
+					<br/>
+
+					<label> Set a password </label>
+					<input type="password" placeholder="Password" name="password" required/>
+					<br/>
+
+					
+					<input type="submit"/>
+				</form>
+
 
 				<LoginButton />
 			</section>
