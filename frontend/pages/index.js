7c892fd79a2e16be979b58c61dcf7a67298772ae
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-09-20 16:51:05 +1000

    Added Button

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,11 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+
 export default function Home() {
 	return (
 		<Layout home>
@@ -9,8 +14,13 @@ export default function Home() {
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
+				<Stack direction="row" spacing={25} align = 'center'>
+					<Button variant="contained">LOGIN</Button>
+					<Button variant="contained">SIGN UP</Button>
+					<Button variant="contained" size="large">ABOUT</Button>
+				</Stack>	
 				<h1>
-					Home
+					"Laughter is brightest in the place where food is good"
 				</h1>
 
 				<p>
