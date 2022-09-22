37a8d84cfb1241901d7a873c4bb8d1e88e952a07
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-09-22 19:14:12 +1000

    fix login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,8 +2,35 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import LoginButton from '../components/loginbutton';
+import {axiosInstance} from '../api/axiosConfig';
 
 export default function Login() {
+	
+	const router = useRouter();
+
+	const submitUser = async (event) => {
+		event.preventDefault();
+		const userName = event.target.userName.value;
+		const password = event.target.password.value;
+
+		const body = {
+			userName: userName,
+			password: password,
+		};
+
+		const url = '/login';
+
+		axiosInstance.post(url, body)
+		.then(function (response) {
+			console.log(response.data);
+			router.push('/restaurant-collection/view-restaurant-collection');
+		})
+		.catch(function (error) {
+			console.log(error);
+		});	
+	
+	}
+
 	const title = `${siteTitle} - Login`;
 	return (
 		<Layout home>
