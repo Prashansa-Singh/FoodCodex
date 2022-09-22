2baf372226f57024c49a54fbe5b5bd580bc7ecef
Prashansa-Singh <88768231+Prashansa-Singh@users.noreply.github.com>
2022-09-22 16:14:00 +1000

    Fix update APIs

--- backend/routes/experience-router.js
+++ backend/routes/experience-router.js
@@ -6,7 +6,7 @@ const experienceController = require('../controllers/experience-controller')
 experienceRouter.get('/view-all', experienceController.getAllExperiences)
 
 experienceRouter.post('/create-one', experienceController.createExperience)
-experienceRouter.post('/update-all', experienceController.updateExperience)
+experienceRouter.post('/update-all', experienceController.updateAllExperiences)
 
 experienceRouter.delete('/delete-one', experienceController.deleteExperience)
 experienceRouter.delete('/delete-all', experienceController.deleteAllExperience)
