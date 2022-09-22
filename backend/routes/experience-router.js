9bdc823af917ff4948bfef9e7cc9a1aa9d267f61
Prashansa-Singh <88768231+Prashansa-Singh@users.noreply.github.com>
2022-09-22 15:14:13 +1000

    Update experience routes
    Update routes to better reflect actual use cases. Will update all and view all together instead of one-by-one.

--- backend/routes/experience-router.js
+++ backend/routes/experience-router.js
@@ -4,10 +4,9 @@ const experienceRouter = express.Router()
 const experienceController = require('../controllers/experience-controller')
 
 experienceRouter.get('/view-all', experienceController.getAllExperiences)
-experienceRouter.get('/view-one', experienceController.getExperience)
 
 experienceRouter.post('/create-one', experienceController.createExperience)
-experienceRouter.post('/update-one', experienceController.updateExperience)
+experienceRouter.post('/update-all', experienceController.updateExperience)
 
 experienceRouter.delete('/delete-one', experienceController.deleteExperience)
 experienceRouter.delete('/delete-all', experienceController.deleteAllExperience)
