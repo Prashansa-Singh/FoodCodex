db29d46d8b7e894ed202462c13fcf2d313e1d72a
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-09-24 11:49:30 +1000

    styling login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import LoginButton from '../components/loginbutton';
+import Button from '@mui/material/Button';
 
 import {axiosInstance} from './api/axiosConfig';
 import axios from 'axios';
@@ -54,12 +55,9 @@ export default function Login() {
 					<label> Set a password </label>
 					<input type="password" placeholder="Password" name="password" required/>
 					<br/>
-
-					
-					<input type="submit"/>
+					<Button className = {utilStyles.homepageButton} variant="contained" size="large">LOGIN</Button>
+					{/* <input type="submit" className='loginButton' /> */}
 				</form>
-
-
 				<LoginButton />
 			</section>
 		</Layout>
