bfc524d2c97a2dabdb8d3777d38852d6f371ae16
yipp-217 <yipp.217@gmail.com>
2022-10-02 19:42:27 +1100

    Generate sharing link

--- backend/controllers/share-controller.js
+++ backend/controllers/share-controller.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/user');
 const { Restaurant } = require('../models/restaurant');
+const { Link } = require('../models/link');
 
 
 const getAllSharedRestaurants = (req, res) => {
@@ -7,10 +8,21 @@ const getAllSharedRestaurants = (req, res) => {
 }
 
 
+
+const generateShareLink = async (req, res, next) => {
+    try {
+        const link = new Link(req.body)
+        await link.save()
+        return res.send(`/user/restaurant/share/public/${link._id}`)
+    }
+    catch(err) {
+        return next(err)
+    }
+}
+
 const shareRestaurant = async (req, res, next) => {
 
 }
-
 
 const shareAllRestaurants = async (req, res, next) => {
 
@@ -19,6 +31,7 @@ const shareAllRestaurants = async (req, res, next) => {
 
 module.exports = {
     getAllSharedRestaurants,
+    generateShareLink,
     shareRestaurant,
     shareAllRestaurants
 }
