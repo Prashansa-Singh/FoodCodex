ecd82179e2dd176a7f4e7454cc6aa60ab47ca9e3
yipp-217 <yipp.217@gmail.com>
2022-10-02 19:43:10 +1100

    Route generate link

--- backend/routes/share-router.js
+++ backend/routes/share-router.js
@@ -3,6 +3,8 @@ const express = require('express')
 const shareRouter = express.Router()
 const shareController = require('../controllers/share-controller')
 
+shareRouter.post('/generate-link', shareController.generateShareLink)
+
 shareRouter.get('/view-all', shareController.getAllSharedRestaurants)
 shareRouter.post('/send-one', shareController.shareRestaurant)
 shareRouter.post('/send-all', shareController.shareAllRestaurants)
