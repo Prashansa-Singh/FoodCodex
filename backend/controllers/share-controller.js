9e2f99d67bf8bc7a54f3ffb0d650819382643604
yipp-217 <yipp.217@gmail.com>
2022-10-02 21:09:19 +1100

    Get share link, duplicate a record with specified fields

--- backend/controllers/share-controller.js
+++ backend/controllers/share-controller.js
@@ -8,15 +8,48 @@ const getAllSharedRestaurants = (req, res) => {
 }
 
 
-
 const generateShareLink = async (req, res, next) => {
     try {
         const link = new Link(req.body)
         await link.save()
+        // make dynamic
         return res.send(`/user/restaurant/share/public/${link._id}`)
     }
-    catch(err) {
+    catch (err) {
         return next(err)
+    }
+}
+
+
+const getShareLink = async (req, res, next) => {
+    try {
+        const link = await Link.findById(req.params.linkId)
+        const record = await Restaurant.findById(link.restaurantId)
+        let restaurant = new Restaurant()
+
+        if (link['shareName'] === true) restaurant.name = record.name;
+        if (link['shareRating'] === true) restaurant.rating = record.rating;
+        if (link['sharePriceRating'] === true) restaurant.priceRating = record.priceRating;
+        if (link['shareCuisine'] === true) restaurant.cuisine = record.cuisine;
+        if (link['shareAddress'] === true) restaurant.address = record.address;
+        if (link['shareOptions'] === true) {
+            restaurant.personalOption = false
+            restaurant.halalOption = record.halalOption
+            restaurant.veganOption = record.veganOption
+            restaurant.vegetarianOption = record.vegetarianOption
+            restaurant.pescatarianOption = record.pescatarianOption
+            restaurant.nutsFreeOption = record.nutsFreeOption
+            restaurant.dairyFreeOption = record.dairyFreeOption
+            restaurant.glutenFreeOption = record.glutenFreeOption
+            restaurant.allergyFriendlyOption = record.allergyFriendlyOption
+            restaurant.diabetesFriendlyOption = record.diabetesFriendlyOption
+        }
+
+        console.log(restaurant)
+        await restaurant.save()
+    }
+    catch (err) {
+        return next (err)
     }
 }
 
@@ -31,7 +64,10 @@ const shareAllRestaurants = async (req, res, next) => {
 
 module.exports = {
     getAllSharedRestaurants,
+
     generateShareLink,
+    getShareLink,
+
     shareRestaurant,
     shareAllRestaurants
 }
