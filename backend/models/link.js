d95bed6e3981a63b59ffe6efdd0783cb43212745
yipp-217 <yipp.217@gmail.com>
2022-10-02 21:06:54 +1100

    Update Link Schema

--- backend/models/link.js
+++ backend/models/link.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 
 const linkSchema = new mongoose.Schema({
-
     senderId: {type: mongoose.Types.ObjectId},
     receiverId: {type: mongoose.Types.ObjectId},
 
@@ -12,10 +11,8 @@ const linkSchema = new mongoose.Schema({
     shareRating: {type: Boolean, default: true},
     sharePriceRating: {type: Boolean, default: true},
     shareCuisine: {type: Boolean, default: true},
-    shareOptions: {type: Boolean, default: true},
-    shareAddress: {type: Boolean, default: false},
-    shareExperiences: {type: Boolean, default: false}
-
+    shareAddress: {type: Boolean, default: true},
+    shareOptions: {type: Boolean, default: false}
 })
 
 
