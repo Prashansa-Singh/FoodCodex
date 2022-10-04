0352475ea8d719309a4c77542a8b87e981f0ef12
yipp-217 <yipp.217@gmail.com>
2022-10-04 23:16:27 +1100

    Refactor extract restaurant function (read from db)

--- backend/controllers/share-controller.js
+++ backend/controllers/share-controller.js
@@ -21,37 +21,44 @@ const generateRestaurantShareLink = async (req, res, next) => {
 
 
 /**
+ * Helper function
+ * Read out the restaurant from DB via provided linkId
+ * Returns: a restaurant object
+ */
+const readSharedRestaurant = async (linkId) => {
+    const link = await Link.findById(linkId)
+    const record = await Restaurant.findById(link.restaurantId)
+
+    let restaurant = {}
+    if (link['shareName'] === true) restaurant.name = record.name;
+    if (link['shareRating'] === true) restaurant.rating = record.rating;
+    if (link['sharePriceRating'] === true) restaurant.priceRating = record.priceRating;
+    if (link['shareCuisine'] === true) restaurant.cuisine = record.cuisine;
+    if (link['shareAddress'] === true) restaurant.address = record.address;
+    if (link['shareOptions'] === true) {
+        restaurant.halalOption = record.halalOption
+        restaurant.veganOption = record.veganOption
+        restaurant.vegetarianOption = record.vegetarianOption
+        restaurant.pescatarianOption = record.pescatarianOption
+        restaurant.nutsFreeOption = record.nutsFreeOption
+        restaurant.dairyFreeOption = record.dairyFreeOption
+        restaurant.glutenFreeOption = record.glutenFreeOption
+        restaurant.allergyFriendlyOption = record.allergyFriendlyOption
+        restaurant.diabetesFriendlyOption = record.diabetesFriendlyOption
+    }
+    return restaurant;
+}
+
+
+/**
  * View a shared restaurant via opening the link
  * open ~/user/restaurant/share/public/linkId
- * Returns: restaurantId, a newly created temporary restaurant record
+ * Returns: a restaurant object with specified shared fields shown
  */
 const viewSharedRestaurant = async (req, res, next) => {
     try {
-        const link = await Link.findById(req.params.linkId)
-        const record = await Restaurant.findById(link.restaurantId)
-        let restaurant = new Restaurant()
-
-        if (link['shareName'] === true) restaurant.name = record.name;
-        if (link['shareRating'] === true) restaurant.rating = record.rating;
-        if (link['sharePriceRating'] === true) restaurant.priceRating = record.priceRating;
-        if (link['shareCuisine'] === true) restaurant.cuisine = record.cuisine;
-        if (link['shareAddress'] === true) restaurant.address = record.address;
-        if (link['shareOptions'] === true) {
-            restaurant.personalOption = false
-            restaurant.halalOption = record.halalOption
-            restaurant.veganOption = record.veganOption
-            restaurant.vegetarianOption = record.vegetarianOption
-            restaurant.pescatarianOption = record.pescatarianOption
-            restaurant.nutsFreeOption = record.nutsFreeOption
-            restaurant.dairyFreeOption = record.dairyFreeOption
-            restaurant.glutenFreeOption = record.glutenFreeOption
-            restaurant.allergyFriendlyOption = record.allergyFriendlyOption
-            restaurant.diabetesFriendlyOption = record.diabetesFriendlyOption
-        }
-
-        console.log(restaurant)
-        await restaurant.save()
-        res.send(restaurant._id)
+        const restaurant = readSharedRestaurant(req.params.linkId)
+        res.send(restaurant)
     }
     catch (err) {
         return next (err)
