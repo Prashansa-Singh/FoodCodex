29e8b9486385ea2fcd0acaa5dfe4e55db57a9fac
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-10-04 17:24:42 +1100

    Fix tag files to make them more readable

--- frontend/components/tag.js
+++ frontend/components/tag.js
@@ -1,9 +1,22 @@
 import styles from './css/tag.module.css';
 
+const tagNames = {
+    personalOption: 'Personal',
+    halalOption: 'Halal',
+    veganOption: 'Vegan',
+    vegetarianOption: 'Vegetarian',
+    pescatarianOption: 'Pescatarian',
+    nutsFreeOption: 'Nut Free',
+    dairyFreeOption: 'Dairy Free',
+    glutenFreeOption: 'Gluten Free',
+    allergyFriendlyOption: 'Allergy Friendly',
+    diabetesFriendlyOption: 'Diabetes Friendly'
+}
+
 export default function Tag({name, colour, displayX}) {
     return (
         <div className={styles.tag} style={{'background-color': colour}} id={name}>
-            <p><b>{name}</b></p>
+            <p><b>{tagNames[name]}</b></p>
             <p id={"x" + name} style={{'display': displayX ? 'flex' : 'none'}} >&#10006;</p>
         </div>
     );
