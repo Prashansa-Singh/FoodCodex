f9d1290d5b0c41e96ce3c8a9c1eb0eb766c309ce
yipp-217 <yipp.217@gmail.com>
2022-10-04 23:52:37 +1100

    Remove routes scaffolding

--- backend/routes/share-router.js
+++ backend/routes/share-router.js
@@ -3,12 +3,9 @@ const express = require('express')
 const shareRouter = express.Router()
 const shareController = require('../controllers/share-controller')
 
+
 shareRouter.post('/generate-link', shareController.generateRestaurantShareLink)
 shareRouter.get('/public/:linkId', shareController.viewSharedRestaurant)
-
-shareRouter.get('/view-all', shareController.getAllSharedRestaurants)
-shareRouter.post('/send-one', shareController.shareRestaurant)
-shareRouter.post('/send-all', shareController.shareAllRestaurants)
 
 
 module.exports = shareRouter;
