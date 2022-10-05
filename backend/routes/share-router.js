11315918827c5c6ef0983dc379b18cbd555bcd77
yipp-217 <yipp.217@gmail.com>
2022-10-06 10:50:22 +1100

    Cors-prove share/generate-link

--- backend/routes/share-router.js
+++ backend/routes/share-router.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 
 const shareRouter = express.Router()
 const shareController = require('../controllers/share-controller')
 
+shareRouter.use(cors())
+shareRouter.options('/generate-link', cors())
 
-shareRouter.post('/generate-link', shareController.generateRestaurantShareLink)
+shareRouter.post('/generate-link', cors(), shareController.generateRestaurantShareLink)
 shareRouter.get('/public/:linkId', shareController.viewSharedRestaurant)
 
 
