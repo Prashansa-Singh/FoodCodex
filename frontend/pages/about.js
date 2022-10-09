271c5f8c383e75b8ab78043749b54f0c6658e4fb
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-10-09 18:23:07 +1100

    made about page

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,6 +1,9 @@
+import { Grid, Paper, Box, Card } from '@mui/material';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 export default function About() {
 	const title = `${siteTitle} - About`;
@@ -10,13 +13,31 @@ export default function About() {
 				<title>{title}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<h1>
-					About
+				<h1 classname = {utilStyles.homepageText}>
+					About Us
+				</h1>
+				<Box>
+					<Paper elevation={10}>
+						<h5>
+							Programming Quokkas are a group of passionate rockstar developers dedicated to bring the best restaurant recording experience for you
+						
+						</h5>
+					
+					</Paper>
+				</Box>
+
+				<h1 classname = {utilStyles.homepageText}>
+					Meet Our Developers 
 				</h1>
 
-				<p>
-					About page
-				</p>
+				<Box>
+					<AccountCircleOutlinedIcon  sx={{ fontSize: 80 }}/>
+					<AccountCircleOutlinedIcon  sx={{ fontSize: 80 }}/>
+					<AccountCircleOutlinedIcon  sx={{ fontSize: 80 }}/>
+					<AccountCircleOutlinedIcon  sx={{ fontSize: 80 }}/>
+					<AccountCircleOutlinedIcon  sx={{ fontSize: 80 }}/>
+				</Box>
+				
 			</section>
 		</Layout>
 	);
