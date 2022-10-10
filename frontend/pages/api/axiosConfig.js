e7c1dfa7886cb65a7d27a7b7f8aa0b0f69fbc60f
callmekelvin <calvin.ho1@outlook.com>
2022-10-10 21:00:47 +1100

    Exposing URL Environment Variables to the Browser

--- frontend/pages/api/axiosConfig.js
+++ frontend/pages/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = (process.env.NODE_ENV == "production") ? process.env.PRODUCTION_BACKEND : "http://localhost:8000/";
+const url = (process.env.NODE_ENV == "production") ? process.env.NEXT_PUBLIC_PRODUCTION_BACKEND : process.env.NEXT_PUBLIC_DEVELOPMENT_BACKEND;
 
 const axiosInstance = axios.create({
     baseURL: url,
