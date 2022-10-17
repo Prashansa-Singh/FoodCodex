2a0bd0e9694e16a968c6469803f10d1abd59ba42
callmekelvin <calvin.ho1@outlook.com>
2022-10-17 11:15:14 +1100

    Secured settings page

--- frontend/pages/restaurant-collection/settings.js
+++ frontend/pages/restaurant-collection/settings.js
@@ -6,13 +6,32 @@ import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import { axiosInstance } from '../api/axiosConfig';
 
+import { getSession } from "next-auth/react"
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/login',
+				permanent: false,
+			},
+		}
+	}
+
+	return {
+		props: {},
+	};
+}
+
 export default function Settings() {
 	const title = `${siteTitle} - Settings`;
 
 	const confirmDelete = () => {
 
 		const userId = '6310539f4af1428a18a7509d';
-		
+
 		const body = {
 			userId: userId,
 		};
@@ -23,34 +42,34 @@ export default function Settings() {
 			title: 'Confirm to delete',
 			message: 'Are you sure you wish to delete all restaurant records from this account?',
 			buttons: [
-			  {
-				label: 'Yes',
-				onClick: () => deleteRestaurants(url, body),
-			  },
-			  {
-				label: 'No',
-			  }
+				{
+					label: 'Yes',
+					onClick: () => deleteRestaurants(url, body),
+				},
+				{
+					label: 'No',
+				}
 			]
 		});
 	}
 
 	const deleteRestaurants = async (url, body) => {
-		await axiosInstance.delete(url, {data: body})
-		.then(function (response) {
-			console.log(response.data);
-			confirmAlert({
-				title: 'Success',
-				message: 'All of the restaurant records were successfully deleted.',
-				buttons: [
-				  {
-					label: 'Ok',
-				  }
-				]
+		await axiosInstance.delete(url, { data: body })
+			.then(function (response) {
+				console.log(response.data);
+				confirmAlert({
+					title: 'Success',
+					message: 'All of the restaurant records were successfully deleted.',
+					buttons: [
+						{
+							label: 'Ok',
+						}
+					]
+				});
+			})
+			.catch(function (error) {
+				console.log(error);
 			});
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
 	}
 
 	return (
