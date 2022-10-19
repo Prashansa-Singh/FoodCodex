7cde6aa03ccc1e47a7b74decdacf64b5b7c98f45
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-10-20 10:14:38 +1100

    Update in workshop

--- frontend/pages/user/restaurant/share/public/[linkId].js
+++ frontend/pages/user/restaurant/share/public/[linkId].js
@@ -3,71 +3,73 @@ import Link from 'next/link';
 import { axiosInstance } from '../../../../api/axiosConfig';
 
 // for actual fetching data that renders on the page
-// export const getServerSideProps = async (context) => {
-//     const {_id} = context.query;
-//     const user = '6310521c744ac9f1587375fa';
-//     const url =  '/user/restaurant/share/public/6350181bd51fc6a3d14e5ab5'
-//     const response = await axiosInstance.get(url, {data: {userId: user, restaurantId: _id,}});
-//     const restaurant_data = response.data;
-// 	const userId = user;
+//export const getServerSideProps = () => {
+    //const {_id} = context.query;
+    //const user = '6310521c744ac9f1587375fa';
+    //const url =  '/user/restaurant/share/public/6350181bd51fc6a3d14e5ab5'
+    //const response = await axiosInstance.get(url);
+    //const restaurant_data = response.data;
+    //console.log(restaurant_data);
+	//const userId = user;
 
-//     console.log("context: ", context);
+    //console.log("context: ", context);
 
 //     return{
-        
+//         props: {},
 //     };
 // }
 export const getStaticPaths = async () => {
     // 1.get id dynamically and concat to /user/restaurant
     // 2. data 
     // 3. presenting 
-    const res = await axiosInstance.get('/user/restaurant/share/public');
-    // const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    // const data = await res.json();
-    console.log('here');
-    console.log('res --->' + res);
-    const data = res.data;
-    console.log('data --->' + data);
-    const paths = data.map(shareData => {
-        // console.log('Error parsing JSON from response:', resClone );
-        return {
-            // linkId needs to be the same as page setup name 
-            params: {linkId: shareData.name.toString() }
-        }
-    })
+    // const res = await axiosInstance.get('/user/restaurant/share/public');
+    // // const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    // // const data = await res.json();
+    // console.log('here');
+    // console.log('res --->' + res);
+    // const data = res.data;
+    // console.log('data --->' + data);
+    // const paths = data.map(shareData => {
+    //     // console.log('Error parsing JSON from response:', resClone );
+    //     return {
+    //         // linkId needs to be the same as page setup name 
+    //         params: {linkId: shareData.name.toString() }
+    //     }
+    // })
+    const paths = [];
 
-    // { fallback: false } means if it's other routes will return 404
+//     // { fallback: false } means if it's other routes will return 404
     return {
         paths, 
         fallback: false
     }
 }
 
-// // ------------------- Note -------------------------
-// // pages are available at build time and ahead of users' request,
-// // might need to check if getServerSideProps is more appropriate here
+// // // ------------------- Note -------------------------
+// // // pages are available at build time and ahead of users' request,
+// // // might need to check if getServerSideProps is more appropriate here
 export const getStaticProps = async (params) => {
-    console.log("context --->" + params);
-    const id = params.linkId; // this was context.params.name
-    console.log('context.param.name --->' + id);
-    // line 30 id should be unique share link 
-    // const res = await fetch('https://jsonplaceholder.typicode.com/users' + id);
-    const res = await axiosInstance.get('/user/restaurant/share/public' + id);
-    // const data = await res.json();
-    const data = res.data;
-    console.log("data in getStaticProps--->" + data);
+    //console.log("context --->" + params);
+    // const id = params.linkId; // this was context.params.name
+    // console.log('context.param.name --->' + id);
+    // // line 30 id should be unique share link 
+    // // const res = await fetch('https://jsonplaceholder.typicode.com/users' + id);
+    // const res = await axiosInstance.get('/user/restaurant/share/public' + id);
+    // // const data = await res.json();
+    // const data = res.data;
+    // console.log("data in getStaticProps--->" + data);
 
     return {
-        props: { shareData: data}
+        props: { }
     }
     
 }
 
-const restaurantDetails = async ({ shareData }) => {
-
+const restaurantDetails = async () => {
+    //console.log(restaurant_data);
     return (
         <div>
-            <h1>{ shareData} </h1>
+            
             <h1>
                 Shared Restaurant
             </h1>
