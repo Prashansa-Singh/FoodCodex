39ae3ad06e2a74a74edd15e6caeafa456a717d43
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-10-19 16:10:47 +1100

    Fix experience view on desktop size

--- frontend/components/experience.js
+++ frontend/components/experience.js
@@ -1,10 +1,18 @@
 import styles from './css/experience.module.css';
 
 export default function Experience({experience}) {
+    const displayTime = () => {
+        console.log(experience.visitTime)
+
+        return (
+            <p>{experience.visitTime}</p>
+        );
+    }
+
     return (
         <div className={styles.experience_container}>
             <p><b>{experience.title}</b></p>
-            <p>{experience.visitTime}</p>
+            {displayTime()}
             <p>{experience.comment}</p>
         </div>
     );
