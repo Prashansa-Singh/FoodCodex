7b18da0d77b99ea0bb9cbe326e0ff2ba2d01fa17
Prashansa-Singh <88768231+Prashansa-Singh@users.noreply.github.com>
2022-10-19 23:59:35 +1100

    Update visitTime value stored in database to Melbourne timezone

--- frontend/components/experience.js
+++ frontend/components/experience.js
@@ -3,9 +3,21 @@ import styles from './css/experience.module.css';
 export default function Experience({experience}) {
     const displayTime = () => {
         console.log(experience.visitTime)
+             
+        let visitTime  = experience.visitTime
+        visitTime = visitTime.substring(0, visitTime.length - 1);
+
+        let visitDate = new Date(visitTime)
+        let hours = visitDate.getHours()
+        let mins = visitTime.substring(visitTime.length - 9, visitTime.length - 7)
+
+        // Convert from 24 hour to 12 hour time
+        let AmOrPm = hours >= 12 ? 'PM' : 'AM';
+        hours = (hours % 12) || 12;
+        let finalTime = hours + ":" + mins + " " + AmOrPm; 
 
         return (
-            <p>{experience.visitTime}</p>
+            <p>{visitDate.toDateString()} {finalTime}</p>
         );
     }
 
