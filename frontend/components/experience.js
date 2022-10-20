c5481250ca0cccc9b90171b0ad79c8cad9957953
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-10-20 15:07:48 +1100

    Add delete experiences

--- frontend/components/experience.js
+++ frontend/components/experience.js
@@ -1,8 +1,10 @@
 import styles from './css/experience.module.css';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { confirmAlert } from 'react-confirm-alert';
+import { axiosInstance } from '../pages/api/axiosConfig';
 
 export default function Experience({experience}) {
     const displayTime = () => {
-        console.log(experience.visitTime)
              
         let visitTime  = experience.visitTime
         visitTime = visitTime.substring(0, visitTime.length - 1);
@@ -21,9 +23,46 @@ export default function Experience({experience}) {
         );
     }
 
+    const confirmDelete = () => {
+
+		const body = {
+			experienceId: experience._id,
+		};
+
+		const url = 'user/restaurant/experience/delete-one';
+
+		confirmAlert({
+			title: 'Confirm to delete',
+			message: 'Are you sure you wish to delete this experience?',
+			buttons: [
+				{
+					label: 'Yes',
+					onClick: () => deleteExperience(url, body),
+				},
+				{
+					label: 'No',
+				}
+			]
+		});
+	}
+
+    const deleteExperience = async (url, body) => {
+        await axiosInstance.delete(url, {data: body})
+        .then(function (response) {
+            console.log(response.data);
+            window.location.reload(); 
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     return (
         <div className={styles.experience_container}>
-            <p><b>{experience.title}</b></p>
+            <div className={styles.topExperienceContainer}>
+                <p><b>{experience.title}</b></p>
+                <DeleteIcon className={styles.bin} onClick={() => confirmDelete()} />
+            </div>
             {displayTime()}
             <p>{experience.comment}</p>
         </div>
