23f91e4408997f3fc8697f0092808d50bb7f79c5
callmekelvin <calvin.ho1@outlook.com>
2022-10-22 10:51:43 +1100

    Adding /account testing

--- backend/app.test.js
+++ backend/app.test.js
@@ -27,7 +27,7 @@ describe('Home ~', () => {
 
 
 /**
- * Account: ~/account
+ * Account: ~/account and User ~/user
  */
 describe('Account ~/account', () => {
     it('POST /signup ---> 201 Created', () => {
@@ -40,12 +40,76 @@ describe('Account ~/account', () => {
             .expect(201)
     })
 
+    it('POST /validate-user ---> Invalid User (null)', () => {
+        request(app).post('/user/validate-user')
+            .send({
+                username: 'NON-EXISTENT-USER',
+                password: 'DOES NOT EXIST',
+            })
+            .expect(200)
+            .expect(null)
+    })
+
+    it('POST /validate-user ---> User (JSON)', () => {
+        request(app).post('/user/validate-user')
+            .send({
+                username: 'X2',
+                password: 'x',
+            })
+            .expect(200)
+            .expect({
+                _id: "6329beec08f04fcd7cd45a8e",
+                userName: 'X2',
+                password: 'x',
+                displayName: 'X',
+                restaurants: [],
+                __v: 0,
+            })
+    })
+
     it('DELETE /delete ---> 204 No Content', () => {
         request(app).delete('/account/delete')
             .send({
                 userName: 'yipp-217'
             })
-            .expect(204)
+            .expect(200)
     })
 })
 
+
+/**
+ * Restaurant Records: ~/user/restaurant
+ */
+//  describe('Restaurant Records: ~/user/restaurant', () => {
+//     it('POST /create-one --->', () => {
+//         request(app).post('/account/signup')
+//             .send({
+
+//             })
+//             .expect(201)
+//     })
+
+//     it('POST /update-one --->', () => {
+//         request(app).post('/account/signup')
+//             .send({
+
+//             }) 
+//             .expect(201)
+//     })
+
+//     it('POST /view-all --->', () => {
+//         request(app).post('/account/signup')
+//             .send({
+
+//             })
+//             .expect(201)
+//     })
+
+//     it('POST /delete-all --->', () => {
+//         request(app).post('/account/signup')
+//             .send({
+
+//             })
+//             .expect(201)
+//     })
+// })
