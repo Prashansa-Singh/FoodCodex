f6a845662f80fbc12b3d1461b495a22cfb0de85a
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-10-23 02:16:55 +1100

    Attempt to add Restaurant in share list

--- frontend/pages/restaurant-collection/share-list.js
+++ frontend/pages/restaurant-collection/share-list.js
@@ -4,6 +4,7 @@ import utilStyles from '../../styles/utils.module.css';
 import { useRef, useState } from 'react';
 import { getSession } from "next-auth/react"
 
+import * as React from 'react';
 import {axiosInstance} from '../api/axiosConfig';
 import Link from 'next/link';
 import Tags from '../../components/tags';
@@ -41,7 +42,7 @@ export async function getServerSideProps(context) {
 	// const user = '6310521c744ac9f1587375fa';
 	// const user = await session.user._id;
 
-	
+
 	// --------------- Single Restaurant Data -------------
 	
 	const url =  'user/restaurant/share/public/'
@@ -75,9 +76,6 @@ export async function getServerSideProps(context) {
 export default function ShareList({ link, restaurantData }) {
 	const title = `${siteTitle} - Share`;
 	const router = useRouter();
-
-	const [shareId, setShareId] = useState(null);
-	let shareLink;
 
 	// // useState of customeShareOptions 
 	// const [options, setOptions] = useState(customShareOptions);
@@ -186,6 +184,71 @@ export default function ShareList({ link, restaurantData }) {
 	console.log("restaurant data type in share " + typeof(restaurantData));
 	console.log("restaurant data in share " + restaurantData);
 
+	// implement add function 
+	const [saveState, setSaveState] = useState(false);
+
+	const [value, setValue] = React.useState(restaurantData.rating);
+	console.log("value " + value);
+
+	const [priceValue, setPriceValue] = React.useState(restaurantData.priceValue);
+	console.log("priceValue " + priceValue);
+
+	const submitEdit = async (event) => {
+		event.preventDefault();
+		const name = event.target.name.value;
+		const cuisine = event.target.cuisine.value;
+		const address = event.target.address.value;
+
+		const body = {
+			// userId: userId,
+			restaurantId: restaurantData._id,
+			name: (name != "") ? name : restaurantData.name,
+			cuisine: (cuisine != "") ? cuisine : restaurantData.cuisine,
+			address: (address != "") ? address : restaurantData.address,
+			rating: value,
+			priceRating: priceValue,
+			// personalOption: event.target.personalOption.value,
+			// halalOption: event.target.halalOption.value,
+			// veganOption: event.target.veganOption.value,
+			// vegetarianOption: event.target.vegetarianOption.value,
+			// pescatarianOption: event.target.pescatarianOption.value,
+			// nutsFreeOption: event.target.nutsFreeOption.value,
+			// dairyFreeOption: event.target.dairyFreeOption.value,
+			// glutenFreeOption: event.target.glutenFreeOption.value,
+			// allergyFriendlyOption: event.target.allergyFriendlyOption.value,
+			// diabetesFriendlyOption: event.target.diabetesFriendlyOption.value,
+		};
+
+		const url = 'user/restaurant/update-one';
+		const urlCreate = 'user/restaurant/create-one';
+		
+		if (saveState){
+			await axiosInstance.post(urlCreate, body)
+			.then(function (response) {
+				console.log("response.data in saveState" + response.data);
+				router.push('/restaurant-collection/view-restaurant-collection');
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		} else {
+			await axiosInstance.post(url, body)
+			.then(function (response) {
+				console.log(response.data);
+				router.push('/restaurant-collection/view-restaurant-collection');
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		}
+
+	}
+
+	
+	const discard = () => {
+		router.push('/restaurant-collection/view-restaurant-collection');
+	}
+	
 	return (
 		<Layout>
 			<Head>
@@ -205,10 +268,10 @@ export default function ShareList({ link, restaurantData }) {
 						</h1>
 
 						<div className={styles.icon_group}>
-							<div className={styles.icons}>
+							<Button onClick={() => setSaveState(true)} className={styles.icons} >
 								<SaveAltIcon/>
 								<p>Save</p>
-							</div>
+							</Button>
 						</div>
 				</div>
 
@@ -234,6 +297,76 @@ export default function ShareList({ link, restaurantData }) {
 				<br />
 				<br />
 				
+				{/* user decides to add the restaurant */}
+				{saveState && (
+						<>
+						<form onSubmit={submitEdit}>
+							{/* <div className={styles.button_container}>
+								<button type='submit' className={styles.submitButton}><b>Save</b></button>
+								<button type='button' className={styles.discardButton} onClick={() => discard()}><b>Discard</b></button>	
+							</div>
+							<div className={styles.editFormTop}>
+								<TextField 
+									id="outlined-restaurant-name" 
+									label="Restaurant Name" 
+									variant="outlined" 
+									name="name" 
+									placeholder={restaurant_data.name}
+									defaultValue={restaurant_data.name}
+									required 
+									margin="dense" 
+									className={styles.textFields}
+								/>
+								<TextField 
+									id="outlined-restaurant-cuisine" 
+									label="Type of Cuisine" 
+									variant="outlined" 
+									name="cuisine" 
+									placeholder={restaurant_data.cuisine}
+									defaultValue={restaurant_data.cuisine}
+									required 
+									margin="dense" 
+									className={styles.textFields}
+								/>
+							</div>
+							<TextField 
+								id="outlined-restaurant-address" 
+								label="Restaurant Address" 
+								variant="outlined" 
+								name="address" 
+								placeholder={restaurant_data.address}
+								defaultValue={restaurant_data.address}
+								required 
+								margin="dense" 
+								className={styles.address}
+							/>
+							<div className={styles.editFormBottom}>
+								<div className={styles.picker}>
+									<label> Rating (out of 5 stars): </label>
+									<Rating precision={0.5} defaultValue={restaurant_data.rating} onChange={(event, newValue) => { setValue(newValue) }} />
+								</div>
+								<div className={styles.picker}>
+									<label> Price Range: </label>
+									<Rating icon={<PaidIcon />} emptyIcon={<PaidOutlinedIcon />} defaultValue={restaurant_data.priceRating} onChange={(event, newPriceValue) => { setPriceValue(newPriceValue) }} />
+								</div>
+							</div>
+							<br/>
+							<label> Tags: </label>
+							<Tags restaurant_data={restaurant_data} page='edit' /> */}
+						</form>
+						<p>Note: Experiences can only be added, updated and deleted from the view restaurant page.</p>
+						<br/>
+						<br/>
+						<br/>
+					</>
+					
+				)}
+				{!saveState &&(
+					<h1>
+						User did not choose to save 
+					</h1>
+
+				)}
 			</section>
 		</Layout>
 	);
