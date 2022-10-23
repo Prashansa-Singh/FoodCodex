1ca4a9d460c73fcbcc29b2c4e4e8980a2f804ec9
callmekelvin <calvin.ho1@outlook.com>
2022-10-23 14:57:04 +1100

    Add View Restaurant CI Test

--- backend/app.test.js
+++ backend/app.test.js
@@ -103,6 +103,25 @@ describe('Account ~/account', () => {
  */
 describe('Restaurant Records: ~/user/restaurant', () => {
     let user;
+    let restaurantId;
+
+    let req_body = {
+        name: "Hakata Gensuke Carlton",
+        cuisine: "Japanese Ramen",
+        address: "126 Lygon Street, Carlton 3053 Victoria",
+        rating: 4.5,
+        priceRating: 3,
+        personalOption: false,
+        halalOption: true,
+        veganOption: true,
+        vegetarianOption: true,
+        pescatarianOption: true,
+        nutsFreeOption: false,
+        dairyFreeOption: false,
+        glutenFreeOption: true,
+        allergyFriendlyOption: false,
+        diabetesFriendlyOption: true,
+    }
 
     beforeAll(async () => {
         let rawUser = await User.findOne({
@@ -123,63 +142,70 @@ describe('Restaurant Records: ~/user/restaurant', () => {
 
         createUser = new User(user_details)
         user = await createUser.save()
+
+        user = user._id.toString()
+        req_body['userId'] = user
     })
 
     it('POST /create-one --->', async () => {
-        const req_body = {
-            userId: user._id.toString(),
-            name: "Hakata Gensuke Carlton",
-            cuisine: "Japanese Ramen",
-            address: "126 Lygon Street, Carlton 3053 Victoria",
-            rating: 4.5,
-            priceRating: 3,
-            personalOption: false,
-            halalOption: true,
-            veganOption: true,
-            vegetarianOption: true,
-            pescatarianOption: true,
-            nutsFreeOption: false,
-            dairyFreeOption: false,
-            glutenFreeOption: true,
-            allergyFriendlyOption: false,
-            diabetesFriendlyOption: true,
-        }
-
         // console.log(req_body.name)
         // console.log(user._id.toString())
 
         const response = await request(app).post('/user/restaurant/create-one').send(req_body)
 
         expect(response.status).toEqual(200);
-        expect(response.text).toEqual(`Done. ${req_body.name} has been added to this userId: ${user._id}.`)
+        expect(response.text).toEqual(`Done. ${req_body.name} has been added to this userId: ${user}.`)
     })
 
-    // it('POST /update-one --->', () => {
-    //     request(app).post('/account/signup')
-    //         .send({
-
-    //         })
-    //         .expect(201)
-    // })
-
-    // it('POST /view-all --->', () => {
-    //     request(app).post('/account/signup')
-    //         .send({
-
-    //         })
-    //         .expect(201)
-    // })
-
-    it('DELETE /delete-one --->', async () => {
-        let userRestaurants = await User.findById(user._id.toString(), "restaurants")
+    it('POST /update-one --->', async () => {
+        let userRestaurants = await User.findById(user, "restaurants")
 
         expect(userRestaurants.restaurants).toEqual(expect.anything())
 
-        let restaurantId;
         if (userRestaurants.restaurants.length >= 0) {
             restaurantId = userRestaurants.restaurants[0].toString()
         }
 
+        expect(typeof restaurantId).toBe("string")
+
+        // update cuisine field of the restaurant
+        let edited_body = Object.assign(req_body, { restaurantId: restaurantId });
+        edited_body["cuisine"] = "Noodles";
+
+        const response = await request(app).post('/user/restaurant/update-one').send(edited_body)
+
+        expect(response.status).toEqual(200)
+        expect(response.text).toEqual(`Done.  userId: ${user} with this restaurantId: ${restaurantId} record has been updated.`)
+    })
+
+    it('GET /view-one --->', async () => {
+        const response = await request(app).get('/user/restaurant/view-one').send({
+            userId: user,
+            restaurantId: restaurantId
+        })
+
+        expect(response.status).toEqual(200);
+
+        let expectedObject = Object.assign(req_body, {
+            experiences: [],
+            allVisits: [],
+            __v: 0,
+        })
+
+        expectedObject['cuisine'] = "Noodles";
+        delete expectedObject.userId
+        delete expectedObject.restaurantId
+
+        let parseResponse = JSON.parse(response.text)
+        delete parseResponse._id
+
+        // console.log(expectedObject)
+        // console.log(parseResponse)
+
+        expect(parseResponse).toMatchObject(expectedObject);
+    })
+
+    it('DELETE /delete-one --->', async () => {
         const response = await request(app).delete('/user/restaurant/delete-one').send({
             restaurantId: restaurantId,
         })
@@ -190,7 +216,7 @@ describe('Restaurant Records: ~/user/restaurant', () => {
 
     afterAll(async () => {
         await request(app).delete('/account/delete').send({
-            userId: user._id.toString(),
+            userId: user,
         })
     })
 })
