bcf4d0d2aeccf02966e0a6560c4ce2de73111572
callmekelvin <calvin.ho1@outlook.com>
2022-10-23 17:14:06 +1100

    Setup Experiences CI Tests

--- backend/app.test.js
+++ backend/app.test.js
@@ -123,6 +123,7 @@ describe('Restaurant Records: ~/user/restaurant', () => {
         diabetesFriendlyOption: true,
     }
 
+    // setup user account
     beforeAll(async () => {
         let rawUser = await User.findOne({
             userName: "test-217",
@@ -147,7 +148,7 @@ describe('Restaurant Records: ~/user/restaurant', () => {
         req_body['userId'] = user
     })
 
-    it('POST /create-one --->', async () => {
+    it('POST /create-one ---> Create Restaurant Record', async () => {
         // console.log(req_body.name)
         // console.log(user._id.toString())
 
@@ -157,7 +158,8 @@ describe('Restaurant Records: ~/user/restaurant', () => {
         expect(response.text).toEqual(`Done. ${req_body.name} has been added to this userId: ${user}.`)
     })
 
-    it('POST /update-one --->', async () => {
+    it('POST /update-one ---> Update Restaurant Record', async () => {
+        // get restaurant id to update record
         let userRestaurants = await User.findById(user, "restaurants")
 
         expect(userRestaurants.restaurants).toEqual(expect.anything())
@@ -172,13 +174,14 @@ describe('Restaurant Records: ~/user/restaurant', () => {
         let edited_body = Object.assign(req_body, { restaurantId: restaurantId });
         edited_body["cuisine"] = "Noodles";
 
+        // update record
         const response = await request(app).post('/user/restaurant/update-one').send(edited_body)
 
         expect(response.status).toEqual(200)
         expect(response.text).toEqual(`Done.  userId: ${user} with this restaurantId: ${restaurantId} record has been updated.`)
     })
 
-    it('GET /view-one --->', async () => {
+    it('GET /view-one ---> Get Restaurant Record', async () => {
         const response = await request(app).get('/user/restaurant/view-one').send({
             userId: user,
             restaurantId: restaurantId
@@ -205,13 +208,85 @@ describe('Restaurant Records: ~/user/restaurant', () => {
         expect(parseResponse).toMatchObject(expectedObject);
     })
 
-    it('DELETE /delete-one --->', async () => {
+    it('DELETE /delete-one ---> Delete Restaurant Record', async () => {
         const response = await request(app).delete('/user/restaurant/delete-one').send({
             restaurantId: restaurantId,
         })
 
         expect(response.status).toEqual(200);
         expect(response.text).toEqual(`Number of documents with restaurantId: ${restaurantId} is [Before Deletion: 1] and [After Deletion: 0].`)
+    })
+
+    afterAll(async () => {
+        await request(app).delete('/account/delete').send({
+            userId: user,
+        })
+    })
+})
+
+describe('Experience Records: ~/user/restaurant/experiences', () => {
+    let user;
+    let restaurantId;
+    let experienceId;
+
+    let create_restaurant_body = {
+        name: "Ho Ho's Canteen",
+        cuisine: "Middle Eastern and Italian",
+        address: "University of Melbourne, Parkville, Melbourne",
+        rating: 4.5,
+        priceRating: 3,
+    }
+
+    let experience_body = {
+        visitTime: Date.now(),
+        title: "First Visit to Ho Ho's Canteen",
+        comment: "One of the better places on campus for fresh, healthy options.",
+    }
+
+    it('Restaurant Experiences Test Setup ---> Create User + Add Restaurant to User', async () => {
+        let rawUser = await User.findOne({
+            userName: "test-514",
+        });
+
+        if (rawUser) {
+            await request(app).delete('/account/delete').send({
+                userId: rawUser._id.toString(),
+            })
+        }
+
+        user_details = {
+            userName: 'test-514',
+            password: '987654321',
+            displayName: '514',
+        }
+
+        // signup user
+        const signup_response = await request(app).post('/account/signup').send(user_details);
+        expect(signup_response.status).toEqual(201)
+
+        let fetchUser = await User.findOne({
+            userName: "test-514",
+        });
+        expect(typeof fetchUser._id).toEqual("object")
+        user = fetchUser._id.toString();
+
+        // create restaurant record for user
+        create_restaurant_body['userId'] = user
+        const add_restaurant = await request(app).post('/user/restaurant/create-one').send(create_restaurant_body)
+        expect(add_restaurant.status).toEqual(200);
+        expect(add_restaurant.text).toEqual(`Done. ${create_restaurant_body.name} has been added to this userId: ${user}.`)
+
+        // get restaurant record id
+        let userRestaurants = await User.findById(user, "restaurants")
+        expect(userRestaurants.restaurants).toEqual(expect.anything())
+
+        if (userRestaurants.restaurants.length >= 0) {
+            restaurantId = userRestaurants.restaurants[0].toString()
+        }
+    })
+
+    it('POST /create-one ---> Add Restaurant Experience', async () => {
+
     })
 
     afterAll(async () => {
