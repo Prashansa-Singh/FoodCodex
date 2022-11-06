b43478d8406aa83dc74af8f12a2fbe033550e669
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2022-11-06 15:56:06 +1100

    fix save button on share

--- frontend/pages/restaurant-collection/share-list.js
+++ frontend/pages/restaurant-collection/share-list.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../../components/layout';
-import { useState } from 'react';
 import { getSession } from "next-auth/react"
 
 import * as React from 'react';
@@ -19,7 +18,6 @@ import 'react-confirm-alert/src/react-confirm-alert.css';
 import { Rating, Modal, Box, Button, Typography, Grid, Paper, TextField, Stack } from "@mui/material";
 import PaidIcon from '@mui/icons-material/Paid';
 import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
-import { confirmAlert } from 'react-confirm-alert';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 
 export async function getServerSideProps(context) {
@@ -37,16 +35,13 @@ export async function getServerSideProps(context) {
 		}
 	}
 
-	// get user id
 	const { _id } = context.query;
-	// const user = '6310521c744ac9f1587375fa';
 	const user = await session.user._id;
 	const userId = user;
 
 
 	// --------------- Single Restaurant Data -------------
 	
-	// get share link id
 	const { link } = context.query;
 	const url =  'user/restaurant/share/public/'
 	const getDataConcatedURL = url + link;
@@ -65,18 +60,9 @@ export default function ShareList({ link, restaurantData, userId}) {
 	const title = `${siteTitle} - Share`;
 	const router = useRouter();
 
-	// --------------------- Presenting Sharing URLs ---------------------------
-
-	// implement add function 
-	const [saveState, setSaveState] = useState(false);
-	const [value, setValue] = React.useState(restaurantData.rating);
-	const [priceValue, setPriceValue] = React.useState(restaurantData.priceValue);
+	// --------------------- Saving shared restaurant ---------------------------
 
 	const submitEdit = async (event) => {
-		// event.preventDefault();
-		// const name = event.target.name.value;
-		// const cuisine = event.target.cuisine.value;
-		// const address = event.target.address.value;
 
 		const body = {
 			userId: userId,
@@ -111,10 +97,6 @@ export default function ShareList({ link, restaurantData, userId}) {
 
 	}
 	
-	const discard = () => {
-		router.push('/restaurant-collection/view-restaurant-collection');
-	}
-	
 	return (
 		<Layout>
 			<Head>
@@ -131,11 +113,10 @@ export default function ShareList({ link, restaurantData, userId}) {
 						<h1>
 							{restaurantData.name}
 						</h1>
-
-						<div className={styles.icon_group}>
-							<Button onClick={() => submitEdit()} className={styles.icons} >
-								<SaveAltIcon/>
-								<p className={shareStyles.textButton}>Save</p>
+						<div >
+							<Button  className={shareStyles.saveButton} onClick={() => submitEdit()} >
+								{/* <SaveAltIcon/> */}
+								<span className={shareStyles.textButton}>Save</span>
 							</Button>
 						</div>
 				</div>
@@ -161,74 +142,6 @@ export default function ShareList({ link, restaurantData, userId}) {
 				<br />
 				<br />
 				<br />
-				
-				{/* user decides to add the restaurant */}
-				{saveState && (
-						<>
-						<form onSubmit={submitEdit}>
-							<div className={shareStyles.formButtons}>
-								<button type='submit' className={shareStyles.submitButton}><b>Save</b></button>
-								<button type='button' className={shareStyles.discardButton} onClick={() => discard()}><b>Discard</b></button>	
-							</div>
-							<div className={editStyles.editFormTop}>
-								<TextField 
-									id="outlined-restaurant-name" 
-									label="Restaurant Name" 
-									variant="outlined" 
-									name="name" 
-									placeholder={restaurantData.name}
-									defaultValue={restaurantData.name}
-									required 
-									margin="dense" 
-									className={editStyles.textFields}
-								/>
-								<TextField 
-									id="outlined-restaurant-cuisine" 
-									label="Type of Cuisine" 
-									variant="outlined" 
-									name="cuisine" 
-									placeholder={restaurantData.cuisine}
-									defaultValue={restaurantData.cuisine}
-									required 
-									margin="dense" 
-									className={editStyles.textFields}
-								/>
-							</div>
-							<TextField 
-								id="outlined-restaurant-address" 
-								label="Restaurant Address" 
-								variant="outlined" 
-								name="address" 
-								placeholder={restaurantData.address}
-								defaultValue={restaurantData.address}
-								required 
-								margin="dense" 
-								className={editStyles.address}
-							/>
-							<div className={shareStyles.editFormBottom}>
-								<div className={shareStyles.picker}>
-									<label> Rating (out of 5 stars): </label>
-									<Rating precision={0.5} defaultValue={restaurantData.rating} onChange={(event, newValue) => { setValue(newValue) }} />
-								</div>
-								<div className={shareStyles.picker}>
-									<label> Price Range: </label>
-									<Rating icon={<PaidIcon />} emptyIcon={<PaidOutlinedIcon />} defaultValue={restaurantData.priceRating} onChange={(event, newPriceValue) => { setPriceValue(newPriceValue) }} />
-								</div>
-							</div>
-							<br/>
-							<label> Tags: </label>
-							<Tags restaurant_data={restaurantData} page='edit' />
-						</form>
-
-						<div>
-							<p>Note: Experiences can only be added, updated and deleted from the view restaurant page.</p>
-						</div>
-						<br/>
-						<br/>
-						<br/>
-					</>
-					
-				)}
 			</section>
 		</Layout>
 	);
